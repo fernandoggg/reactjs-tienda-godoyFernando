c24b443d0b8979c32dffcd497c5ed828c45127e1
fernandoggg <fernando.guillermog@gmail.com>
2022-12-20 15:43:02 -0300

    prueba

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
         <nav className="navegation">
             <img className="logo" src={imglogo} alt="logo"/>
             <ul className="list">
-                <li><a href="">Inicio</a></li>
+                <li><a href="">Inicios</a></li>
                 <li><a href="">Nosotros</a></li>
                 <li><a href="">Tienda</a></li>
             </ul>
